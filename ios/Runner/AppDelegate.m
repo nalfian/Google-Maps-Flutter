f29192397d351c2df5f8f7d96aaf0c59f61aeda4
Nur Alfian Julianda <nalfianjuli@gmail.com>
2020-01-18 13:37:34 +0700

    add gmaps in ios

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,12 +1,12 @@
 #import "AppDelegate.h"
 #import "GeneratedPluginRegistrant.h"
+#import "GoogleMaps/GoogleMaps.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
     [GMSServices provideAPIKey: @"AIzaSyADYx5gyo1JlYkdgRKGbHgkvWlfkDowrlU"];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
